2656deb9e6aef99e2e3e403904bd1632bdb2d06e
Brendan Briggs <br.br.cat@gmail.com>
2014-02-02 22:54:19 -0500

    Changed spacing, added 'n more...'

--- app/components/run-loop-queue.js
+++ app/components/run-loop-queue.js
@@ -3,7 +3,20 @@ var RunLoopQueue = Ember.Component.extend({
   classNameBindings: ['active'],
   active:function(){
 	return this.get('parentView.controller.content.hasItems') && this.get('queue.name') === this.get('parentView.controller.content.currentQueue');
-  }.property('parentView.controller.content.currentQueue','parentView.controller.content.hasItems')
+  }.property('parentView.controller.content.currentQueue','parentView.controller.content.hasItems'),
+
+  shortenedItems: function(){
+    var queueItems = this.get('context.queue.actions'); 
+      return (queueItems || []).slice(0,2);
+  }.property('context.queue.actions.length'),
+  moreItems:function(){
+  	return this.get('context.queue.actions.length') > 2;
+  }.property('context.queue.actions.length'),
+
+  moreItemsLength:function(){
+  	return this.get('context.queue.actions.length') - 2;
+  }.property('context.queue.actions.length'),
+
 });
 
 export default RunLoopQueue;
